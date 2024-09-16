e6a75596b9624ad28e3867c2e3a0db3fc5a7e6fe
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-09-16 01:08:58 -0600

    Add optimization_experiment

--- optimalStoppingAlgos.py
+++ optimalStoppingAlgos.py
@@ -1,38 +1,82 @@
 import random
 import matplotlib.pyplot as plt
 
-len_candidates = 100
-solution_found_count = {}
-optimal_solution_found_count = {}
-for i in range(1, len_candidates):
-    solution_found_count[str(i)] = 0
-    optimal_solution_found_count[str(i)] = 0
+###############################
+# Problem 1
+###############################
 
+def uniform_optimal_stopping(len_candidates, num_of_experiments):
+    print("Sampling uniform distribution data....")
+    optimal_solution_found_count = {}
+    for i in range(1, len_candidates):
+        optimal_solution_found_count[str(i)] = 0
 
+    for experiment in range(num_of_experiments):
+        candidates = random.sample(range(0,1000), len_candidates)
+        optimal_candidate = max(candidates)
 
-for experiment in range(1000):
-    candidates = random.sample(range(0,1000), len_candidates)
-    optimal_candidate = max(candidates)
+        for i in range(1, len_candidates):
+            for candidate in candidates[i:-1]:
+                if candidate > max(candidates[0:i]):
+                    if candidate == optimal_candidate:
+                        optimal_solution_found_count[str(i)] += 1
+
+                    break
 
     for i in range(1, len_candidates):
-        for candidate in candidates[i:-1]:
-            if candidate > max(candidates[0:i]):
-                solution_found_count[str(i)] += 1
-                if candidate == optimal_candidate:
-                    optimal_solution_found_count[str(i)] += 1
-                
-                break
+        optimal_solution_found_count[str(i)] = round((optimal_solution_found_count[str(i)]/num_of_experiments) * 100, 3)
+
+    return optimal_solution_found_count
+
+def slide_back(sY):
+    maxIndex = 0
+    for i in range(len(sY)):
+        if sY[i] > sY[maxIndex]:
+            maxIndex = i
+    bestRatio = 0
+    bestIndex = maxIndex
+    for i in range(maxIndex, 0, -1):
+        ratio = 0
+        if (sY[maxIndex] - sY[i]) != 0:
+            ratio = (maxIndex - i) / (sY[maxIndex] - sY[i])
+        if ratio >= bestRatio:
+            bestRatio = ratio
+            bestIndex = i
+    return bestIndex + 1
+
+def optimization_experiment(num_of_experiments):
+    count = {}
+
+    for i in range(1, 100):
+            count[str(i)] = 0
+
+    for experiment in range(num_of_experiments):
+            sample = uniform_optimal_stopping(100,1000)
+            x1, y1 = zip(*sample.items())
+            opSample = slide_back(y1)
+
+            count[str(opSample)] += 1
+    
+    return count
+
+def plot_sample(sample):
+    x, y = zip(*sample.items())
+
+    plt.plot(x,y)
+    plt.xticks(fontsize = 8)
+    plt.show()
 
 
+s1 = uniform_optimal_stopping(100,10000)
+# s2 = optimization_experiment(10000)
 
-print(candidates)
-print(optimal_candidate)
+plot_sample(s1)
 
-x, y = zip(*optimal_solution_found_count.items())
+###############################
+# Problem 2
+###############################
 
-print(x)
-print(y)
-print(optimal_solution_found_count)
-plt.plot(x,y)
-plt.show()
+###############################
+# Problem 3
+###############################
 
